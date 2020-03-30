e852adea039937aacc4c0ae1e814bfaa6ae8d99c
DeMarcus Kirby <tkd024@gmail.com>
2020-03-30 11:02:13 -0600

    add search test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'As a user of the search feature', type: :feature do
-  visit '/'
-  fill_in :q, with: "sweet potatoes"
-  click_on "Search"
+  it 'should search for videos' do
+    visit '/'
+    fill_in :q, with: "sweet potatoes"
+    click_on "Search"
 
-  expect(current_path).to eq("/foods")
-  expect(page).to have_content("Number of results: 10")
+    expect(current_path).to eq("/foods")
+    expect(page).to have_content("Number of results: 50")
+
+    expect(page).to have_content("GTIN/UPC code: 492111402857")
+    expect(page).to have_content("Description: SWEET POTATOES")
+    expect(page).to have_content("Brand Owner: ARCHER FARMS")
+    expect(page).to have_content("Ingredients: SWEET POTATOES")
+
+  end
 end
